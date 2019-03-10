3cdc9fe7dc94f2d0ba19f46e6b9067b3a559fd1d
SuperNeal666 <1175796298@qq.com>
2019-03-10 20:02:17 +0800

    作业

---
+++ 2.17/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ std::cout <<"a:"<<endl;
+  std::cout <<"1 2 3 4"<<endl;
+  std::cout <<"b:"<<endl;
+  std::cout <<"1 "<<"2 "<<"3 "<<"4"<<endl;
+   std::cout <<"c:"<<endl;
+    std::cout << "1 ";
+     std::cout <<"2 ";
+     std::cout <<"3 ";
+     std::cout <<"4";
+
+    return 0;
+}
