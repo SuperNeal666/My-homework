0dad31d63e2fc33f7266c05b8484e83c26cc5a12
SuperNeal666 <1175796298@qq.com>
2019-03-10 20:24:41 +0800

    no message

---
+++ 2.18.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+    cout <<"Enter two integers";
+    cout <<"Enter the first integer x =";
+    cin >> x;
+    cout <<"Enter the second integer y =";
+    cin >> y;
+    if (x<y)
+    cout <<y<<"is large";
+    if (x>y);
+    cout <<x<<"is large";
+    if (x=y);
+    cout <<"These numbers are equal."<<endl;
+    return 0;
+
+}
