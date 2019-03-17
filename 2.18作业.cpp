19e01f93e0cd6bd42ffd2a537dcf9fc04299f775
SuperNeal666 <1175796298@qq.com>
2019-03-17 10:45:35 +0800

    作业

---
+++ 2.18作业.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if(a>b)
+    cout<<a<<"is large";
+    else if(a<b)
+    cout<<b<<"is large";
+    else
+    cout<<"These numbers are equal.";
+    return 0;
+    }
